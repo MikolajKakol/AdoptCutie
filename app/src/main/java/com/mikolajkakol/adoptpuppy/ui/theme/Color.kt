c277e561c826547bb5edbc77b4d11d66329877e2
Mikołaj Kąkol <mkakol@speednet.pl>
2021-02-24 19:30:20 +0100

    added proper icon

--- app/src/main/java/com/mikolajkakol/adoptpuppy/ui/theme/Color.kt
+++ app/src/main/java/com/mikolajkakol/adoptpuppy/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.mikolajkakol.adoptpuppy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Accent = Color(0xFFD7EEFE)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
